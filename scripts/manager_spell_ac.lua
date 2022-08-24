272f738f9d27f3933139508f40bda62f9e3d1182
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2022-08-24 15:29:41 +0200

    Add working resetSpells and resetPrepared

---
+++ scripts/manager_spell_ac.lua
@@ -0,0 +1,44 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+local resetSpellsOriginal;
+local resetPreparedOriginal;
+local tSpellsets = { "spellset", "itemspellset", "otherspellset" };
+
+function onInit()
+    resetSpellsOriginal = SpellManager.resetSpells;
+    SpellManager.resetSpells = resetSpells;
+
+    resetPreparedOriginal = SpellManager.resetPrepared;
+    SpellManager.resetPrepared = resetPrepared;
+end
+
+-- Reset power points and individual spells cast
+function resetSpells(nodeCaster)
+    for _,sSpellset in ipairs(tSpellsets) do
+        for _,nodeSpellClass in pairs(DB.getChildren(nodeCaster, sSpellset)) do
+            DB.setValue(nodeSpellClass, "pointsused", "number", 0);
+            
+            for _,nodeLevel in pairs(DB.getChildren(nodeSpellClass, "levels")) do
+                for _,nodeSpell in pairs(DB.getChildren(nodeLevel, "spells")) do
+                    DB.setValue(nodeSpell, "cast", "number", 0);
+                end
+            end
+        end 
+    end
+end
+
+-- Iterate through each spell to reset
+function resetPrepared(nodeCaster)
+    for _,sSpellset in ipairs(tSpellsets) do
+        for _,nodeSpellClass in pairs(DB.getChildren(nodeCaster, sSpellset)) do
+            for _,nodeLevel in pairs(DB.getChildren(nodeSpellClass, "levels")) do
+                for _,nodeSpell in pairs(DB.getChildren(nodeLevel, "spells")) do
+                    DB.setValue(nodeSpell, "prepared", "number", 0);
+                end
+            end
+        end
+    end
+end
