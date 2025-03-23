b610db7b36b094c4b1be1c903faa1d4bc7f0bbdf
Simon-Raphael Fischer <50429899+Kelrugem@users.noreply.github.com>
2025-03-23 19:00:17 +0800

    Update char_main.lua
    Compatibilities with newest changes. I added missing functions like locking the character sheet :) Some other missing functions were added, too, but you may have deleted them on purpose, so please revise :)

--- campaign/scripts/char_main.lua
+++ campaign/scripts/char_main.lua
@@ -6,11 +6,52 @@
 NODETOTALHP = "hp.total";
 
 function onInit()
-	onSystemChanged();
+	self.onLevelChanged();
+	self.onSystemChanged();
+	self.onLockModeChanged(WindowManager.getWindowReadOnlyState(self));
+
+	DB.addHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", self.onLevelChanged);
 
 	onLiveHP();
 	onDrainPermanentBonus();
 	onMaladyTracker();
+end
+
+function onClose()
+	DB.removeHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", self.onLevelChanged);
+end
+
+function onLockModeChanged(bReadOnly)
+	local tFieldsAbility = { "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma", };
+	local tFieldsAbilityBonus = { "strengthbonus", "dexteritybonus", "constitutionbonus", "intelligencebonus", "wisdombonus", "charismabonus", };
+	local tFieldsAbilityDamage = { "strengthdamage", "dexteritydamage", "constitutiondamage", "intelligencedamage", "wisdomdamage", "charismadamage", };
+	local tFieldsHealth = { "hp", };
+	--local tFieldsHealth = { "wounds", "hptemp", "nonlethal", };
+	local tFieldsOther = { "speedfinal", "speedspecial", "senses", };
+
+	WindowManager.callSafeControlsSetLockMode(self, tFieldsAbility, bReadOnly);
+	WindowManager.callSafeControlsSetLockMode(self, tFieldsAbilityBonus, bReadOnly);
+	WindowManager.callSafeControlsSetLockMode(self, tFieldsAbilityDamage, bReadOnly);
+	WindowManager.callSafeControlsSetLockMode(self, tFieldsHealth, bReadOnly);
+	WindowManager.callSafeControlsSetLockMode(self, tFieldsOther, bReadOnly);
+
+	if UtilityManager.getTopWindow(self).getClass() == "charsheetmini" then
+		local tFieldsCombat = { "initiative", "melee", "ranged", "grapple", };
+		local tFieldsDefense = { "ac", "srfinal", "fortitude", "reflex", "will", };
+		WindowManager.callSafeControlsSetLockMode(self, tFieldsCombat, bReadOnly);
+		WindowManager.callSafeControlsSetLockMode(self, tFieldsDefense, bReadOnly);
+	else
+		local tFieldsTop = { "race", };
+		local tFieldsCombat = { "initiative", "meleemainattackbonus", "rangedmainattackbonus", "grappleattackbonus", }
+		local tFieldsDefense = { "dr", "ac", "spellresistance", "fortitude", "reflex", "will", };
+		WindowManager.callSafeControlsSetLockMode(self, tFieldsTop, bReadOnly);
+		WindowManager.callSafeControlsSetLockMode(self, tFieldsCombat, bReadOnly);
+		WindowManager.callSafeControlsSetLockMode(self, tFieldsDefense, bReadOnly);
+	end
+end
+
+function onLevelChanged()
+	CharManager.calcLevel(getDatabaseNode());
 end
 
 function onHealthChanged()
@@ -35,8 +76,10 @@ function onSystemChanged()
 	if label_grapple then
 		if bPFMode then
 			label_grapple.setValue(Interface.getString("cmb"));
-		else
+		elseif minisheet then
 			label_grapple.setValue(Interface.getString("grp"));
+		else
+			label_grapple.setValue(Interface.getString("grapple"));
 		end
 	end
 	
@@ -49,6 +92,16 @@ function onSystemChanged()
 
 	perception.setVisible(bPFMode);
 	label_perception.setVisible(bPFMode);
+end
+
+function onDrop(x, y, draginfo)
+	if draginfo.isType("shortcut") then
+		local sClass, sRecord = draginfo.getShortcutData();
+		if StringManager.contains({"referenceclass", "referencerace"}, sClass) then
+			CharManager.addInfoDB(getDatabaseNode(), sClass, sRecord);
+			return true;
+		end
+	end
 end
 
 function onLiveHP()
